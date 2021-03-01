04bbc0dedcf303c2504cb0a7984df3ac0560371b
jimchou <r0842584@student.vives.be>
2021-03-01 10:04:26 +0100

    Counter added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,5 +34,13 @@ namespace EersteProjectGitHub
         {
             lblHello.Content = "Hello World";
         }
+
+        int count;
+        private void btnCounter_Click(object sender, RoutedEventArgs e)
+        {
+            count++;
+
+            lblCounter.Content = count;
+        }
     }
 }
